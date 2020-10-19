5bd5f0baa44ee8ba240f110bf472506015375df6
jilmy <55695536+jilmy@users.noreply.github.com>
2020-10-19 23:51:28 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,5 +1,12 @@
 package org.devops
 //打印消息内容
-def PrintMes(content){
-    println(content)
+def PrintMes(value,color){
+    colors = ['red' : "\033[40;31m >>>>>>>>>> ${value} <<<<<<<<<< 033[0m",
+              'yellow' : "\033[33m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m",
+              'blue' : "\033[40;31m >>>>>>>>>> ${value} <<<<<<<<<< 033[0m",
+              'green' : "\033[32m >>>>>>>>>> ${value} <<<<<<<<<< 033[0m",
+              'black' : "\033[30m >>>>>>>>>> ${value} <<<<<<<<<< 033[0m" ]
+    ansiColor{'xterm'} {
+        println(colors[color])
+    }
 }
