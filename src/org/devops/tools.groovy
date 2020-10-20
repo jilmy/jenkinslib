0e8ad7615a9c9b13c7d2e489e023ebecb11aaa87
jilmy <55695536+jilmy@users.noreply.github.com>
2020-10-20 13:53:24 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,5 +1,5 @@
 package org.devops
-//格式化输出Printmes
+//格式化输出PrintMes
 def PrintMes(vals,cor) {
     cors = ['red' : "\033[40;31m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m",
               'yellow' : "\033[33m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m",
