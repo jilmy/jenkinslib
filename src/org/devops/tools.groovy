de3520061355622353b1304174d134a200c0bb21
jilmy <55695536+jilmy@users.noreply.github.com>
2020-10-20 13:37:31 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -1,13 +1,13 @@
 package org.devops
 //格式化输出
-def PrintMes(value,color){
-    colors = ['red' : "\033[40;31m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m",
+def PrintMes(vals,cor) {
+    cors = ['red' : "\033[40;31m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m",
               'yellow' : "\033[33m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m",
               'blue' : "\033[40;31m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m",
               'green' : "\033[32m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m",
               'black' : "\033[30m >>>>>>>>>> ${value} <<<<<<<<<< \033[0m" ]
     ansiColor('xterm') {
-        println(colors[color])
+        println(cors[cor])
     }
 }
 
